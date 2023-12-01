783b8d39df77df8a5bcdd3c8c59970af281e4623
ray <ray1997ray75@gmail.com>
2023-12-01 20:57:58 +0800

    change dec 1

--- src/pages/carpool/CarpoolLaunch.jsx
+++ src/pages/carpool/CarpoolLaunch.jsx
@@ -1,6 +1,9 @@
 import { useEffect,useState } from "react";
 import {NavLink, Outlet, useLoaderData, Link, Form, useNavigate} from "react-router-dom";
 // 下拉式選單 人數 , 共乘方式
+
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 export const CarpoolLaunch = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
 
@@ -12,24 +15,68 @@ export const CarpoolLaunch = () => {
   const [ end, SetEnd] = useState('');
   const [ otherlocate,setOtherlocate] = useState('');
   const [ otherlocations, setOtherlocations] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(null);
 
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
 
   const url_initiate_carpool = "http://localhost:8000/initiate-carpool-event-ui"
 
 
 
   const PrintOtherLocation=() =>{
-      return{
-        
-      }
+
+    return otherlocations.map((item) => {
+      
+      return (
+        <div key={item.place}>
+          <p> 地點：{item.place}</p>
+          <hr />
+        </div>
+      );
+    });
   }
 
   const HandleLaunchclick = () =>{
-      
+
+    var target = {
+      "user_id": 1,
+      "start_time": "2023-12-20T07:22:38.757Z",
+      "self_drive_or_not": true,
+      "number_of_people": 6,
+      "start_location": "台北",
+      "end_location": "桃園",
+      "other_location": [
+        ""
+      ]
+
+    }
+    fetch(url_initiate_carpool,{
+
+      method : 'POST' ,
+      headers: new Headers({
+          'accept' :  'application/json',
+          'Content-Type' : 'application/json' // <-- Specifying the Content-Type
+      }),body : JSON.stringify(target)
+      }) // <-- Post Parameters
+      .then((response) => response.json())
+      .then((responseText) => {
+          //console.log(responseText);
+          setlaunchresult(responseText);
+          //setEventdata(responseText);
+          console.log(responseText);
+          
+          //alert(responseText);
+      })
+      .catch((error) => {
+          
+          console.error(error);
+      });
 
   }
 
-  
+
   // store the Otherlocations by click button
   const HandleOtherLocation = () => {
 
@@ -48,12 +95,20 @@ export const CarpoolLaunch = () => {
     }, []); // Transport one empty tuple as second parameter to insure useEffect as Componet only work once
     
   const formattedDate = currentDate.toLocaleTimeString();
+
   return (
 
     
     <div>
       <h1>發起共乘</h1>
       <p>現在時間 :{formattedDate}</p>
+      <h2>Select Date and Time</h2>
+      <DatePicker
+        selected={selectedDate}
+        onChange={handleDateChange}
+        showTimeSelect
+        dateFormat="Pp"
+      />
       <hr />
       <label for="number_of_people">共乘人數：</label> 
       <input type="text" id="number_of_people" name="number_of_people" placeholder="請輸入共乘人數"
@@ -86,7 +141,7 @@ export const CarpoolLaunch = () => {
       <button onClick={HandleOtherLocation}>確認</button>
       <br />
       <label for="end_loc">已新增中間上下車地點：</label> 
-      {PrintOtherLocation}
+      {PrintOtherLocation()}
     </div>
      
   );
