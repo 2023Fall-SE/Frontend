068e5ce540195019abc99ce18e874fb840eda1d6
CY <esnh0076@gmail.com>
2023-12-11 07:48:20 +0800

    fix e2e testing bugs v0.6

--- e2e_test.py
+++ e2e_test.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-
-driver = webdriver.Chrome()
+service = Service(executable_path='./chromedriver.exe')
+driver = webdriver.Chrome(service=service)
 driver.get('http://localhost:3000')
 print('Page Title:', driver.title)
 driver.quit()
