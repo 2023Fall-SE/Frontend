3c492a7172053b622aa041691a440738fb143f3e
CY <esnh0076@gmail.com>
2023-12-11 07:31:23 +0800

    fix e2e testing bugs v0.4

--- e2e_test copy.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-
-driver = webdriver.Chrome()
-driver.get('http://localhost:3000')
-print('Page Title:', driver.title)
-driver.quit()
--- e2e_test.py
+++ e2e_test.py
@@ -1,11 +1,7 @@
 from selenium import webdriver
 
-chrome_driver_path = '/chromedriver'
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-
+driver = webdriver.Chrome()
 driver.get('http://localhost:3000')
-
-
 print('Page Title:', driver.title)
 driver.quit()
