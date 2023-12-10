067f38fd49e8368473b4302ae02523bb5f88da2d
CY <esnh0076@gmail.com>
2023-12-11 07:59:42 +0800

    fix e2e testing bugs v0.8

--- e2e_test.py
+++ e2e_test.py
@@ -2,7 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 service = Service(executable_path='./chromedriver')
-driver = webdriver.Chrome(service=service)
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headlessl') 
+
+driver = webdriver.Chrome(service=service,options=chrome_options)
 driver.get('http://localhost:3000')
 print('Page Title:', driver.title)
 driver.quit()
