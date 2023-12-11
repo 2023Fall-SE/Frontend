1baa832c00d103cceacaf0a1945a30176f91dfaf
CY <esnh0076@gmail.com>
2023-12-11 08:07:37 +0800

    fix e2e testing bugs v1.0

--- e2e_test.py
+++ e2e_test.py
@@ -6,7 +6,7 @@ service = Service(executable_path='./chromedriver')
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headlessl') 
+chrome_options.add_argument('--headless') 
 
 driver = webdriver.Chrome(service=service,options=chrome_options)
 driver.get('http://localhost:3000')
